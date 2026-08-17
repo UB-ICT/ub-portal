ae0146e891d10140519e389c286441cda4883304
KingxJames <james.faber@ub.edu.bz>
2026-08-17 08:55:48 -0600

    fixed dates and isMultiCostCenter because it was undefined,

--- src/features/purchase-order-requisition/components/RunningBalanceChart.tsx
+++ src/features/purchase-order-requisition/components/RunningBalanceChart.tsx
@@ -59,6 +59,9 @@ const costCenterIds = Array.from(
   new Set(data.map((point) => point.cost_center_id))
 ).sort((a, b) => a - b)
 
+  const dates = Array.from(new Set(data.map((point) => point.date))).sort()
+  const isMultiCostCenter = costCenterIds.length > 1
+
   let option: EChartsOption
 
   if (!isMultiCostCenter && dates.length > 0) {
